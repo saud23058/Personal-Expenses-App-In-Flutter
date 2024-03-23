a31f3591044e9c8109976b9d31fa131f727d0720
HP <ksaud3508@gmail.com>
2024-03-24 00:19:27 +0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Flutter"),),
+      body: Column(
+        children: [
+          Card(child: Text("Chart"),),
+          Card(child: Text("Transactions"),)
+        ],
+      ),
+    );
+  }
+}
+
